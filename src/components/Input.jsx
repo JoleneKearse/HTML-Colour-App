5de41c56916dc6a3a1896b0c3d8eb518113473ea
Jolene Kearse <pharaohnutz@gmail.com>
2022-10-09 17:31:07 -0400

    added Input component & styling

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,9 +1,25 @@
+import "../css/Input.css";
+
 export default function Input() {
   return (
     <>
       <div className="container">
         <form>
-          <h2>Input</h2>
+          <label
+            htmlFor="htmlInput"
+            className="sr-only"
+          >
+            Enter a HTML colour name:
+          </label>
+          <input
+            type="text"
+            name="htmlInput"
+            id="htmlInput"
+            className="html-input"
+            placeholder="Enter a HTML colour name..."
+            autoFocus
+            required
+          />
         </form>
       </div>
     </>
